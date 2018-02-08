2f3b423b2d8b9adbccf69a3d356e94545ee76d53
Mahfuzur-Rahman <7mahfuzur.rahman@gmail.com>
2018-02-09 01:33:54 +0600

    Employee attendance Web Application Project starts

---
+++ Employee attendance Web/Employee attendance Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Employee_attendance_Web.MvcApplication" Language="C#" %>
---
+++ Employee attendance Web/Employee attendance Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Employee_attendance_Web.Startup))]
+namespace Employee_attendance_Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
